736e3cbb0d95818ff482cf58686bb4932bd31eaf
Nick <nickpaliyev@gmail.com>
2023-04-16 22:13:46 -0400

    Created the first Home page of the website with a side bar at the top that comes out with different options. with my name and profile pic with under it being a carousel of different projects im working the lower portion is all the projects with more information and will be updated properly soon. lastly the bottom that still needs work otherwise it is very basic.

---
+++ Main.js
@@ -0,0 +1,88 @@
+function openNav() {
+    document.getElementById("sidebar").style.width = "150px";
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+}
+
+function updateImgSize() {
+    const Img = document.getElementById("image-content");
+    const containerWidth = Img.parentElement.clientWidth;
+    Img.style.width = containerWidth + "px";
+    Img.style.height = containerWidth + "px";
+}
+
+window.addEventListener("resize", updateImgSize);
+updateImgSize();
+
+
+const images = ["Budget Chart.jpeg", "FuturesBot.jpeg", "PetCare.jpeg"];
+const texts = ["Budget And Projection Calulator", "Futures Trading Bot", "Pet Care Software"];
+
+let currentIndex = 0;
+let contentInterval;
+
+function startContentInterval() {
+    contentInterval = setInterval(changeContent, 6000);
+}
+
+function resetContentInterval() {
+    clearInterval(contentInterval);
+    startContentInterval();
+}
+
+function setContent() {
+    const imageElement = document.getElementById("image-content");
+    const textElement = document.getElementById("text-content");
+    const dots = document.getElementsByClassName("dot");
+
+    imageElement.style.opacity = 0;
+    textElement.style.opacity = 0;
+
+    setTimeout(() => {
+        imageElement.src = images[currentIndex];
+        textElement.innerHTML = texts[currentIndex];
+        imageElement.style.opacity = 1;
+        textElement.style.opacity = 1;
+
+        for (let i = 0; i < dots.length; i++) {
+            dots[i].classList.remove("active");
+        }
+        dots[currentIndex].classList.add("active");
+    }, 1000); // Wait for the transition to complete
+}
+
+function scrollToRowWithOffset(element, offset) {
+    const y = element.getBoundingClientRect().top + window.pageYOffset + offset;
+    window.scrollTo({ top: y, behavior: 'smooth' });
+}
+
+function scrollToRow() {
+    const rowId = currentIndex === 0 ? 'row1' : (currentIndex === 1 ? 'row2' : 'row3');
+    const rowElement = document.getElementById(rowId);
+    const offset = -100;
+    scrollToRowWithOffset(rowElement, offset);
+}
+
+document.getElementById("text-content").addEventListener("click", scrollToRow);
+
+function changeContent() {
+    currentIndex = (currentIndex + 1) % images.length;
+    setContent();
+}
+
+function nextContent() {
+    currentIndex = (currentIndex + 1) % images.length;
+    setContent();
+    resetContentInterval();
+}
+
+function previousContent() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    setContent();
+    resetContentInterval();
+}
+
+startContentInterval(); // Start the interval
+
